ff9ad73e4e2e6aef0a924e7ddc22b8f8d4ffec6f
LAPTOP-HIBQ8KAL\sander <sanderlahoz123@gmail.com>
2026-09-20 20:01:39 +0200

    Refactor to stream data directly
    Removed the buffer to stream data directly from client (proxy) to server and back.
    
    changed the code to handle all connections concurrently in goroutines.

--- 02-reverse-proxy/proxy/main.go
+++ 02-reverse-proxy/proxy/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 )
@@ -10,44 +10,63 @@ import (
 const originServer = "127.0.0.1:8080"
 
 func main() {
-
-	// Start tcp server
+	// Start TCP listener on port 3000
 	listener, err := net.Listen("tcp", "127.0.0.1:3000")
 	if err != nil {
-		log.Fatal("Failed to start server", err)
+		log.Fatal("Failed to start server: ", err)
 	}
-	fmt.Println("Listening on port 3000")
+	// Close connection when main exits
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			log.Fatal("Failed to close listener: ", err)
+		}
+	}(listener)
 
-	// Accept connections
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Fatal("Failed to accept connection", err)
+	fmt.Println("Proxy listening on port 3000...")
+
+	// Infinite event loop that waits for client connections
+	for {
+		clientConn, err := listener.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection: ", err)
+			continue
+		}
+
+		// Handle each client separately in their own goroutine
+		go handleConnection(clientConn)
 	}
+}
 
-	// Wrap connection with buffered IO
-	// we can now read the data directly from this reader
-	reader := bufio.NewReader(conn)
+// A function to handle a client connection
+func handleConnection(clientConn net.Conn) {
+	// Close connection when function exits
+	defer func(clientConn net.Conn) {
+		err := clientConn.Close()
+		if err != nil {
+			log.Println("Failed to close connection: ", err)
+		}
+	}(clientConn)
 
-	// We read data from the reader into this buffer in memory
-	buffer := make([]byte, 4096)
-	_, err = reader.Read(buffer)
+	// Establish connection to origin server (8080)
+	originConn, err := net.Dial("tcp", originServer)
 	if err != nil {
-		fmt.Println("Error reading from connection to buffer", err)
+		log.Println("Failed to connect to originServer: ", err)
 		return
 	}
+	// Close connection when function exits
+	defer func(originConn net.Conn) {
+		err := originConn.Close()
+		if err != nil {
+			log.Println("Failed to close originServer: ", err)
+		}
+	}(originConn)
 
-	// Contact origin server to establish connection
-	OriginConn, err := net.Dial("tcp", originServer)
-	if err != nil {
-		log.Fatal("Failed to connect to originServer", err)
-	}
+	// Send client request -> origin server (in a goroutine)
+	go func() {
+		_, _ = io.Copy(originConn, clientConn)
+	}()
 
-	// Write the buffer to the origin server
-	// (Forward message to the server through this proxy)
-	_, err = OriginConn.Write(buffer)
-	if err != nil {
-		log.Fatal("Failed to write to originServer", err)
-		return
-	}
-
+	// Send origin response -> client
+	_, _ = io.Copy(clientConn, originConn)
 }
