5ad5a5ca574799b723a97b394fb45bb0c4c708c5
LAPTOP-HIBQ8KAL\sander <sanderlahoz123@gmail.com>
2026-09-20 16:24:46 +0200

    Setup conn and buffered reading
    Listener listening on port 3000 conn handled using reader and stored in a buffer of 4096 bytes. Printed to console on GET 1/1.

--- 02-reverse-proxy/main.go
+++ 02-reverse-proxy/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+
+	// Start tcp server
+	listener, err := net.Listen("tcp", "127.0.0.1:3000")
+	if err != nil {
+		log.Fatal("Failed to start server", err)
+	}
+	fmt.Println("Listening on port 3000")
+
+	// Accept connections
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatal("Failed to accept connection", err)
+	}
+
+	// Wrap connection with buffered IO
+	// we can now read the data directly from this reader
+	reader := bufio.NewReader(conn)
+
+	// We read data from the reader into this buffer in memory
+	buffer := make([]byte, 4096)
+	_, err = reader.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading from connection to buffer", err)
+		return
+	}
+
+	fmt.Println(string(buffer))
 }
